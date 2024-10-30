0bd3ee4490f4cba120fdea2992f5e725fe0b4445
MDisdog <xiaoduxiaodu317@gmail.com>
2024-10-30 15:43:12 +0800

    refractor: initial commit

---
+++ KeyMapper/KeyMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyMapperApp.swift
+//  KeyMapper
+//
+//  Created by 杜昊霖 on 2024/10/30.
+//
+
+import SwiftUI
+
+@main
+struct KeyMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyMapperTests/KeyMapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyMapperTests.swift
+//  KeyMapperTests
+//
+//  Created by 杜昊霖 on 2024/10/30.
+//
+
+import Testing
+@testable import KeyMapper
+
+struct KeyMapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
